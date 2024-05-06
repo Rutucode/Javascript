d48996f1649d40a6cb886635e42eae04d917e557
Rutucode <rutujapawarhere08@gmail.com>
2024-05-06 23:50:39 +0530

    PracticeCode/JavaScriptKeywords16.html         PracticeCode/JsVriables17.html

--- Freecode/variables.js
+++ Freecode/variables.js
@@ -155,3 +155,123 @@ let c = 33;
 a /= 12;
 b /= 4;
 c /= 11;
+
+//Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+/*Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters. */
+
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+/*Assign the following three lines of text into the single variable myStr using escape sequences.
+
+FirstLine
+    \SecondLine
+ThirdLine
+You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+Note: The indentation for SecondLine is achieved with the tab escape character, not spaces.
+
+ */
+//const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+
+//const myStr = "This is the start. " + "This is the end."; // Change this line
+
+/*Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string. */
+
+//let myStr;
+myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//Set myName to a string equal to your name and build myStr with myName between the strings My name is  and  and I am well!
+
+// Only change code below this line
+const myName = "Zuri";
+//const myStr = "My name is" + myName + "and I am well";
+
+//Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+// Change code below this line
+const someAdjective = "fun";
+//let myStr = "Learning to code is ";
+myStr += someAdjective ;
+
+//Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+console.log(lastName.length);
+
+//Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+// Setup
+let firstLetterOfLastName = "";
+//const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+// Setup
+//let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+// Setup
+//const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//PRB: Use bracket notation to find the last character in the lastName variable.
+// Setup
+//const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//Use bracket notation to find the second-to-last character in the lastName string.
+// Setup
+//const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+/*In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence. */
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "It was raining when a " + myAdjective +" "+ myNoun +" "+ myVerb + " " + myAdverb + " in front of a bike"; // Change this line
+// Only change code above this line 
+
+//Modify the new array myArray so that it contains both a string and a number (in that order)
+// Only change code below this line
+//const myArray = ["There", 3, "peanut butter", "sandwitches" ];
+
+//Create a nested array called myArray.
+// Only change code below this line
+//const myArray = [[23, "bulls"], [2, "cows"], [5, "chikens"]];
+
+//Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+const myArray = [50, 60, 70];
+const myData = myArray[0];
