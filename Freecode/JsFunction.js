bc27a4d28bc881e2142fdd2219b0d6e07cc33431
Rutucode <rutujapawarhere08@gmail.com>
2024-05-15 20:30:38 +0530

    Freecode/JsFunction.js

--- Freecode/JsFunction.js
+++ Freecode/JsFunction.js
@@ -664,6 +664,173 @@ switch (val) {
 
 chainToSwitch(7);
 //-------------------------------------------------------------------------------------
+//Fix the function isLess to remove the if/else statements.
+function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15);
+/*Ans */
+function isLess(a, b) {
+  // Only change code below this line
+
+    return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+//-------------------------------------------------------------------------------------
+/*Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+Hint
+Remember that undefined is a keyword, not a string. */
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if(a < 0 || b < 0)
+return undefined;
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+//-------------------------------------------------------------------------------------
+/*In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+Count Change	Cards
++1	2, 3, 4, 5, 6
+0	7, 8, 9
+-1	10, 'J', 'Q', 'K', 'A'
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Outputs: -3 Hold or 5 Bet
+
+Hint
+Do NOT reset count to 0 when value is 7, 8, or 9.
+Do NOT return an array.
+Do NOT include quotes (single or double) in the output. */
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  var msg = '';
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  if (count === 5) msg = "5 Bet";
+  else if (count === 0) msg = "0 Hold";
+  else if (count === -5) msg = "-5 Hold";
+  else if (count === -1) msg = "-1 Hold";
+  else if (count === 1) msg = "1 Bet";
+
+
+  return msg;
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+//-------------------------------------------------------------------------------------
+/*Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array. */
+const myDog = {
+  // Only change code below this line
+"name" : "dog",
+"legs": 4,
+  "tails": 1,
+  friends: ["pup", "maddy"]
+  // Only change code above this line
+};
+//-------------------------------------------------------------------------------------
+//Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+//-------------------------------------------------------------------------------------
+/*Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively. */
+// Setup
+/* 
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+*/
+//-------------------------------------------------------------------------------------
+//Set the playerNumber variable to 16. Then, use the variable to look up the player's name and assign it to player.
+/*
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+*/
+
+//-------------------------------------------------------------------------------------
+/*Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.*/
+/*
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog["name"] = "Happy Coder";
+*/
+//-------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------
