560db4ad2d05755d3a8e500fe988fb7f619e5f99
Rutucode <rutujapawarhere08@gmail.com>
2024-06-12 23:02:07 +0530

    Scrimba/chromeExtn/index.js Scrimba/chromeExtn/index.js

--- Scrimba/chromeExtn/index.js
+++ Scrimba/chromeExtn/index.js
@@ -14,7 +14,7 @@ for (let i = 0; i < myLeads.length; i++){
     console.log( myLeads[i])
 }
 */
-
+/*
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -37,7 +37,49 @@ for (let i = 0; i < myLeads.length; i++) {
     // ulEl.append(li)
 }
 
+*/
+
+//--------------------------------------------------------------------------------
+/*
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+// 1. Save a key-value pair in localStorage
+localStorage.getItem("myLeads", "www.lead.com");
+// 2. Refresh the page. Get the value and log it to the console
+console.log(localStorage.getItem("myLeads"))
+// 3. Clear localStorage
+localStorage.clear()
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
 
 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+*/
+//--------------------------------------------------------------------------------
 
 
