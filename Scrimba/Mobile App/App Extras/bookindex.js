810324b8ef3a0810a38264ed8385aa4660acd545
Rutucode <rutujapawarhere08@gmail.com>
2024-06-25 19:18:50 +0530

    shopping list app

--- Scrimba/Mobile App/App Extras/bookindex.js
+++ Scrimba/Mobile App/App Extras/bookindex.js
@@ -1,30 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://playground-c5b18-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://playground-987e5-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const booksInDB = ref(database, "books")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const booksInDB = ref(database, "books");
 
-const booksEl = document.getElementById("books")
+const booksEl = document.getElementById("books");
+//onValue function runs everytime there is edit to database
 
-onValue(booksInDB, function(snapshot){
-    let booksArray = Object.values(snapshot.val())
-     
-    // Challenge: Write a for loop where you console log each book.
+onValue(booksInDB, function (snapshot) {
+  let booksArray = Object.values(snapshot.val());
+  clearBooksListEl(); // clears before starting to append
 
-    for(let i = 0; i < booksArray; i++){
-        
-    }
-})
+  // Challenge: Write a for loop where you console log each book.
+
+  for (let i = 0; i < booksArray.length; i++) {
+    currentBooks = booksArray.length[i];
+    // console.log(currentbooks)
+
+    // Challenge: Use the appendBookToBooksListEl() function to append book instead of console logging
+
+    appendBookToBooksListEl(currentBooks); // bookValu is not here so we substitue it with currentBooks
+  }
+});
 
 function clearBooksListEl() {
-    booksEl.innerHTML = ""
+  booksEl.innerHTML = "";
 }
 
 function appendBookToBooksListEl(bookValue) {
-    booksEl.innerHTML += `<li>${bookValue}</li>`
-}
+  booksEl.innerHTML += `<li>${bookValue}</li>`;
+} // bookValue is parameter of function
--- Scrimba/Mobile App/index.js
+++ Scrimba/Mobile App/index.js
@@ -5,12 +5,14 @@ import {
   getDatabase,
   ref,
   push,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; // Use your own URL
+  onValue,
+  remove
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL:
     "https://playground-987e5-default-rtdb.asia-southeast1.firebasedatabase.app/",
-};
+}; // Use your own URL
 
 const app = initializeApp(appSettings); // pass argument from other file
 const database = getDatabase(app);
@@ -21,9 +23,10 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
-let inputValue = document.getElementById("input-field");
+let inputFieldEl = document.getElementById("input-field");
 
 const addToCartBtn = document.getElementById("add-button");
+let shoppingListEl = document.getElementById("shopping-list");
 
 addToCartBtn.addEventListener("click", () => {
   let inputValue = inputFieldEl.value;
@@ -39,13 +42,50 @@ addToCartBtn.addEventListener("click", () => {
 
   // Challenge: Append a new <li> with text content inputValue to the 'shopping-list' <ul>
 
-  let addShoppingList = document.getElementById("shopping-list");
-
-  addShoppingList.innerHTML += `<li> ${inputValue} <li>`; //append previous items
+  shoppingListEl.innerHTML += `<li> ${inputValue} <li>`; //append previous items
 
   emptyInputValue();
-  addToList(inputValue); //use parameter
+  // addToList(inputValue); //use parameter
 });
+
+/*
+Challenge:
+Call the onValue function with
+shoppingListInDB as the first argument and
+function(snapshot) {} as the second argument
+*/
+onValue(shoppingListInDB, function (snapshot) {
+  //console.log(snapshot.val())
+
+  // Challenge: Use Object.values() to convert snapshot.val() from an Object to an Array. Create a variable for this.
+
+
+  let snapshotValuArr = Object.entries(snapshot.val());
+
+  clearShoppingListEl();
+
+  //console.log(snapshotValuArr)
+
+  // Challenge: Write a for loop to iterate on itemsArray and console log each item
+  for (let i = 0; i < snapshotValuArr.length; i++) {
+   
+    // Challenge: Use the appendItemToShoppingListEl(itemValue) function inside of the for loop to append item to the shopping list element for each iteration.
+    let itemArry = snapshotValuArr[i];
+     // Challenge: Make two let variables:
+        // currentItemID and currentItemValue and use currentItem to set both of
+    // them equal to the correct values.
+    
+    let currentItemID = itemArry[0]
+    let currentItemValue = itemArry[1]
+    addToList(itemArry);
+    //console.log(snapshotValuArr[i]);
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
 // Challenge: Refactor the line below into its own function.
 // Challenge: Refactor the line below into its own function.
 
@@ -53,6 +93,24 @@ function emptyInputValue() {
   inputFieldEl.value = "";
 }
 
-function addToList(inputValue) {
-  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+function addToList(item) {
+ // shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+ let itemId = item[0]
+ let itemValue = item[1]
+
+//create Element
+let newItem = document.createElement("li")
+   
+    // Challenge: Make it so the item name shows instead of 'Something'
+    newItem.textContent = inputValue
+     // Challenge: Attach an event listener to newEl and make it so you console log the id of the item when it's pressed.
+    newItem.addEventListener("click", () =>{
+     // Challenge: Make a let variable called 'exactLocationOfItemInDB' and set it equal to ref(database, something) where you substitute something with the code that will give you the exact location of the item in question.
+     let exactLocationOfItemInDB = ref(database, `shoppingList/${itemId}`)
+      // Challenge: Use the remove function to remove the item from the database
+     remove(exactLocationOfItemInDB)
+    }) // give full location/path - shoppingList/${itemId}
+
+    shoppingListEl.append(newItem)
+
 } //Append a new <li> function || //use parameter as value is coming from top
